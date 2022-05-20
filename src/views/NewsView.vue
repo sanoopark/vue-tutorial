7b36096c4c5dc745d032aa527f9cba54d8df30a0
sanoopark <sanoopark@gmail.com>
2022-05-20 17:35:14 +0900

    add news api

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>News</div>
+  <div>{{ users }}</div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios
+      .get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => console.log(error));
+  },
+};
 </script>
 
 <style></style>
